4249de246ef04a8b0b03c4dfbcaddbc5b25c3600
ReylakSishe <145812190+ReylakSishe@users.noreply.github.com>
2023-09-27 20:10:30 -0300

    A

--- Proyectos/Organismos/organismos.py
+++ Proyectos/Organismos/organismos.py
@@ -1,76 +1,64 @@
-#listas
+# listas
 
-logones = ["B959263","C569853","T698952","Z698563"]
+logones = ["B959263", "C569853", "T698952", "Z698563"]
 
-roles = ["superior","adjunto","temporario","tercerizado"]
+roles = ["superior", "adjunto", "temporario", "tercerizado"]
 
-contraseñas = ["CHIPI1971","EURASIA","JAJAJANT","ECHENME"]
+contraseñas = ["CHIPI1971", "EURASIA", "JAJAJANT", "ECHENME"]
 
-#intentos
+# intentos
 
-intento = 0
-intentoHecho = intento + 1
-intento = intentoHecho
 
-#funciones
+# funciones
+
 
 def terminar():
     print("sin intentos")
 
 
 def usuarioLogueado():
-    logonIngresado = input("ingrese su logon")
-    if logonIngresado in logones:
-        print("logon correcto")
-        rolAsignado()
-        
-    else:
-        print("logon incorrecto")
-        
-        if intento > 3:
-            print("ya no podes ingresar")
-            terminar()
+    intento = 0
+    while intento < 3:
+        logonIngresado = input("ingrese su logon")
+        if logonIngresado in logones:
+            print("logon correcto")
+            rolAsignado()
 
         else:
-            print ("intente de nuevo por favor")
-            usuarioLogueado()
-            intento()
+            print("logon incorrecto")
+            intento += 1
+    if intento == 3:
+        terminar()
+
 
 def rolAsignado():
-    rolIngresado = input("ingrese su rol")
-    if rolIngresado in roles:
-        print("rol correcto")
-        contraseñaPuesta()
-        
-    else:
-        print("rol incorrecto")
-        
-        if intento > 3:
-            print("ya no podes ingresar")
-            terminar()
+    intento = 0
+    while intento < 3:
+        rolIngresado = input("ingrese su rol")
+        if rolIngresado in roles:
+            print("rol correcto")
+            contraseñaPuesta()
 
         else:
-            print ("intente de nuevo por favor")
-            usuarioLogueado()
-            intento()
-            
+            print("rol incorrecto")
+            intento += 1
+    if intento == 3:
+        terminar()
+
 
 def contraseñaPuesta():
-    contraseñaIngresada = input("ingrese su contraseña")
-    if contraseñaIngresada in contraseñas:
-        print("contraseña correcta, bienvenido de vuelta")
-        
-    else:
-        print("contraseña incorrecta")
-
-        if intento > 3:
-            print("ya no podes ingresar")
-            terminar()
+    intento = 0
+    while intento < 3:
+        contraseñaIngresada = input("ingrese su contraseña")
+        if contraseñaIngresada in contraseñas:
+            print("contraseña correcta")
+            print("bienvenido de vuelta")
 
         else:
-            print("intente de nuevo por favor")
-            usuarioLogueado()
-            intento()
+            print("contraseña incorrecta")
+            intento += 1
+    if intento == 3:
+        terminar()
 
 
 usuarioLogueado()

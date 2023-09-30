c3e903f15c63e82a7645b841abcc6dee7b2b0735
ReylakSishe <145812190+ReylakSishe@users.noreply.github.com>
2023-09-30 11:34:10 -0300

    Update Calculadora.py

--- Proyectos/Calculadora/Calculadora.py
+++ Proyectos/Calculadora/Calculadora.py
@@ -1,10 +1,19 @@
-
-
 def calcular():
-    a = int (input())
-    b = int (input())
-    c = a + b
-    print(c)
+    operacionElegida = input("que operacion quiere realizar ")
+    a = int(input())
+    b = int(input())
+    suma = a + b
+    resta = a - b
+    multiplicacion = a * b
+    division = a / b
+    if operacionElegida == "suma":
+        print(suma)
+    if operacionElegida == "resta":
+        print(resta)
+    if operacionElegida == "multiplicacion":
+        print(multiplicacion)
+    if operacionElegida == "division":
+        print(division)
     calcular()
 
 

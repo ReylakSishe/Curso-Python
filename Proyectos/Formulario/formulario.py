f39497f40fbe4a19e7cfb6d184fc4f099c28612e
ReylakSishe <145812190+ReylakSishe@users.noreply.github.com>
2023-10-02 19:24:55 -0300

    Create formulario.py

---
+++ Proyectos/Formulario/formulario.py
@@ -0,0 +1,14 @@
+def form1():
+    input(str("Nombre: "))
+    input(str("Apelllido: "))
+    input(str("Direccion: "))
+    input(date("Fecha de acto: "))
+    input(int("Numero de oficina: "))
+    input(str("Logon: "))
+    input(str("Provincia: "))
+    input(str("Localidad: "))
+    input(("Codigo postal: "))
+    input(("Numero de DNI: "))
+
+
+form1()

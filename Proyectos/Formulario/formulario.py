280b4b3d9a2e6d2c5e7383ecece119bb506a88b6
ReylakSishe <145812190+ReylakSishe@users.noreply.github.com>
2023-10-02 20:25:48 -0300

    Update formulario.py

--- Proyectos/Formulario/formulario.py
+++ Proyectos/Formulario/formulario.py
@@ -1,14 +1,34 @@
+from datetime import date
+
+
 def form1():
-    input(str("Nombre: "))
-    input(str("Apelllido: "))
-    input(str("Direccion: "))
-    input(date("Fecha de acto: "))
-    input(int("Numero de oficina: "))
-    input(str("Logon: "))
-    input(str("Provincia: "))
-    input(str("Localidad: "))
-    input(("Codigo postal: "))
-    input(("Numero de DNI: "))
+    nombre = str(input("Nombre: "))
+    apellido = str(input("Apelllido: "))
+    direccion = str(input("Direccion: "))
+    fechaDeActo = int(input("Fecha de acto (AÑO-MES-DIA): "))
+    numeroDeOficina = int(input("Numero de oficina: "))
+    logon = str(input("Logon: "))
+    provincia = str(input("Provincia: "))
+    localidad = str(input("Localidad: "))
+    codigoPostal = int(input("Codigo postal: "))
+    NumeroDNI = int(input("Numero de DNI: "))
+
+    try:
+        fechaDeActo = date.fromisoformat(fechaDeActo)
+    except:
+        print("Ingrese la fecha en el formato indicado (AÑO-MES-DIA)")
+
+    print("Su acto quedo asi:")
+    print("Nombre: ", nombre)
+    print("Apellido: ", apellido)
+    print("Direccion: ", direccion)
+    print("Fecha de acto: ", fechaDeActo)
+    print("Numero de oficina: ", numeroDeOficina)
+    print("Logon", logon)
+    print("Provincia: ", provincia)
+    print("Localidad: ", localidad)
+    print("Codigo postal: ", codigoPostal)
+    print("Numero de DNI: ", NumeroDNI)
 
 
 form1()
